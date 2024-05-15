1a4c2533bb725267e9bd365273bab98e3eab1ce0
Gamboii <127925737+ST10085670@users.noreply.github.com>
2024-05-15 16:37:39 +0200

    Final

--- prjOnlineDB/Program.cs
+++ prjOnlineDB/Program.cs
@@ -10,23 +10,26 @@ namespace prjOnlineDB
             {
 
                 string connectionString = "Server=tcp:st10085670.database.windows.net,1433;Initial Catalog=st10085670OnlineDB;Persist Security Info=False;User ID=courseserverst10085670;Password=Thandi2001;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-                using (SqlConnection connection = new SqlConnection(connectionString)) 
+                using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     connection.Open();
-                    Console.WriteLine("======================");
+                    Console.WriteLine("=============================");
                     Console.WriteLine("Student in the class!");
-                    Console.WriteLine("======================");
+                    Console.WriteLine("=============================");
                     String sql = "Select * from StudentMarks";
                     using (SqlCommand command = new SqlCommand(sql, connection))
                     {
-                        using (SqlDataAdapter reader = command.ExecuteReader()) 
+                        using (SqlDataReader reader = command.ExecuteReader())
                         {
-                            while (reader.Read()) 
-                            { Console.WriteLine("{0} - {1} {2}", reader.GetInt32(0), reader.GetString(1), reader.GetString(2)); }
-                            
+                            while (reader.Read())
+                            {
+                                Console.WriteLine("{0}-{1} {2}", reader.GetInt32(0), reader.GetString(1), reader.GetString(2));
+                            }
                         }
-                    }                
+                    }
+
                 }
+
             }
             catch (Exception ex) 
             {
